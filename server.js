cf067198c2279d58a4858230ddb6ffacea1368b7
Tiago Arrosi <twarrosi@gmail.com>
2017-06-26 21:19:05 -0300

    Add post todos

--- server.js
+++ server.js
@@ -1,20 +1,27 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
+var todoNextId = 1;
 
-var todos = [{
-  id: 1,
-  description: 'Meet mon for lunch',
-  completed: false
-}, {
-  id: 2,
-  description: 'Go to market',
-  completed: false
-}, {
-  id: 3,
-  description: 'Go to crossfit',
-  completed: true
-}];
+app.use(bodyParser.json());
+
+var todos = [];
+
+// var todos = [{
+//   id: 1,
+//   description: 'Meet mon for lunch',
+//   completed: false
+// }, {
+//   id: 2,
+//   description: 'Go to market',
+//   completed: false
+// }, {
+//   id: 3,
+//   description: 'Go to crossfit',
+//   completed: true
+// }];
 
 app.get('/', function(req, res) {
   res.send('Todo API Root');
@@ -41,6 +48,15 @@ app.get('/todos/:id', function(req, res) {
   } else {
     res.status(404).send();
   }
+});
+
+app.post('/todos', function(req, res) {
+  var body = req.body;
+
+  body.id = todoNextId++;
+  todos.push(body);
+
+  res.json(body);
 });
 
 app.listen(PORT, function() {
