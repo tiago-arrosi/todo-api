ffbf3c4f90145667272c4b2a4f44fe9ffcf44851
Tiago Arrosi <twarrosi@gmail.com>
2017-06-27 21:57:33 -0300

    add put todos

--- server.js
+++ server.js
@@ -61,6 +61,47 @@ app.post('/todos', function(req, res) {
   res.json(body);
 });
 
+// DELETE /todos/:id
+app.delete('/todos/:id', function(req, res) {
+  var todoId = parseInt(req.params.id, 10);
+  var matchedTodo = _.findWhere(todos, {id:todoId});
+
+  if (matchedTodo) {
+    todos = _.without(todos, matchedTodo);
+    res.json(matchedTodo);
+  } else {
+    res.status(404).json({"error":"no todo found with that id"});
+  }
+});
+
+//PUT /todos/:id
+app.put('/todos/:id', function(req, res) {
+  var todoId = parseInt(req.params.id, 10);
+  var body = _.pick(req.body, 'description', 'completed');
+  var matchedTodo = _.findWhere(todos, {id:todoId});
+  var validAttributes = {};
+
+  if (!matchedTodo) {
+    return res.status(404).json({"error":"no todo found with that id"});
+  }
+
+  if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
+    validAttributes.completed = body.completed;
+  } else if (body.hasOwnProperty('completed')) {
+    res.status(400).json({"error": "completed must be a boolean value"})
+  }
+
+  if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
+    validAttributes.description = body.description;
+  } else if (body.hasOwnProperty('description')) {
+    res.status(400).json({"error": "description must be a string value"})
+  }
+
+  _.extend(matchedTodo, validAttributes);
+  res.json(matchedTodo);
+
+});
+
 app.listen(PORT, function() {
   console.log('Express listening on port ' + PORT + '!!');
 });
