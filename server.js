05c1827a143d80a7d794ca4d01bace30e80737d2
Tiago Arrosi <twarrosi@gmail.com>
2017-07-03 21:28:55 -0300

    convert get /todos/:id to sequelize

--- server.js
+++ server.js
@@ -53,15 +53,28 @@ app.get('/todos', function(req, res) {
 //GET /todos/:id
 app.get('/todos/:id', function(req, res) {
   var todoId = parseInt(req.params.id, 10);
-  var matchedTodo = _.findWhere(todos, {
-    id: todoId
-  });
 
-  if (matchedTodo) {
-    res.json(matchedTodo);
-  } else {
-    res.status(404).send();
-  }
+  db.todo.findById(todoId).then(function(todo) {
+    if (!!todo) {
+      res.json(todo.toJSON());
+    } else {
+      res.status(404).json({
+        "error": "no todo found with that id"
+      });
+    }
+  }).catch(function(e) {
+    res.status(500).send();
+  });;
+
+  // var matchedTodo = _.findWhere(todos, {
+  //   id: todoId
+  // });
+
+  // if (matchedTodo) {
+  //   res.json(matchedTodo);
+  // } else {
+  //   res.status(404).send();
+  // }
 });
 
 // POST /todos
@@ -73,22 +86,12 @@ app.post('/todos', function(req, res) {
   }).catch(function(e) {
     res.status(400).json(e);
   })
-
-  // if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
-  //   return res.status(400).send();
-  // }
-
-  // body.description = body.description.trim();
-
-  // body.id = todoNextId++;
-  // todos.push(body);
-
-  // res.json(body);
 });
 
 // DELETE /todos/:id
 app.delete('/todos/:id', function(req, res) {
   var todoId = parseInt(req.params.id, 10);
+
   var matchedTodo = _.findWhere(todos, {
     id: todoId
   });
