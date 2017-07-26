129dbfb9743902cf9b8aae892a530ee4258ff1b6
Tiago Arrosi <twarrosi@gmail.com>
2017-07-25 21:53:19 -0300

    add auth middleware and findByTokin class method

--- models/user.js
+++ models/user.js
@@ -63,7 +63,29 @@ module.exports = function(sequelize, dataTypes) {
           }, function(e) {
             return reject();
           });
-        })
+        });
+      }, findByToken: function(token) {
+        return new Promise(function (resolve, reject) {
+          try {
+            var decodedJWT = jwt.verify(token, 'qwerty098');
+            var bytes = cryptojs.AES.decrypt(decodedJWT.token, 'abc123!@#');
+            var tokenData = JSON.parse(bytes.toString(cryptojs.enc.Utf8));
+
+            user.findById(tokenData.id).then(function() {
+              if (user) {
+                resolve(user);
+              } else {
+                reject();
+              }
+            }, function(e) {
+              reject();
+            });
+
+          } catch (e) {
+            reject();
+          }
+
+        });
       }
     },
     instanceMethods: {
